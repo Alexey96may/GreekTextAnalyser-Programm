6f20c7ae49ba3d217bb2eb18275c7e7a38811c0e
Alexey96may <shulga_alexey@vk.com>
2025-10-31 11:55:14 +0300

    complete main logic

--- classes/StringWorker.ts
+++ classes/StringWorker.ts
@@ -1,6 +1,12 @@
+import Word from "../types/Word";
+import WordStructure from "../types/WordStructure";
+import * as fs from "fs";
+import { Sort } from "../types/Sort";
+
 export class StringWorker {
     allWordsArr: string[];
-    vocals = [
+
+    vocals: string[] = [
         "α",
         "ά",
         "ε",
@@ -17,12 +23,61 @@ export class StringWorker {
         "ώ",
     ];
 
-    articles = [
+    exept: string[] = [
+        "φου",
+        "χει",
+        "ότε",
+        "οπότε",
+        "όποτε",
+        "όχι",
+        "ώπα",
+        "υπό",
+        "τος",
+        "ουσ",
+        "ναι",
+        "κει",
+        "εδώ",
+        "ας",
+        "αχ",
+        "γι",
+        "δε",
+        "δάπ",
+        "δω",
+        "δώ",
+        "εε",
+        "εμ",
+        "επί",
+        "θα",
+        "καν",
+        "κι",
+        "μεν",
+        "μες",
+        "μω",
+        "να",
+        "πάρ",
+        "πιο",
+        "πλετ",
+        "πω",
+        "ρε",
+        "τόσο",
+        "τότε",
+        "τόσες",
+        "τόσα",
+        "χω",
+        "ωχ",
+        "όνο",
+        "όπως",
+        "αμ",
+    ];
+
+    articles: string[] = [
         "ο",
         "η",
         "το",
         "του",
         "της",
+        "την",
+        "τον",
         "τη",
         "ένας",
         "ένα",
@@ -37,7 +92,140 @@ export class StringWorker {
         "μιας",
     ];
 
-    nounEndings = [
+    conjunc: string[] = [
+        "αν",
+        "ενώ",
+        "αλλά",
+        "και",
+        "ακόμα",
+        "όταν",
+        "έτσι",
+        "αλλιώς",
+        "ωστόσο",
+        "πάντως",
+        "όμως",
+        "που",
+        "πού",
+        "ότι",
+        "πως",
+        "πώς",
+        "μην",
+        "μήπως",
+        "όταν",
+        "μόλις",
+        "πριν",
+        "αφού",
+        "καθώς",
+        "μέχρι",
+        "ώσπου",
+        "γιατί",
+        "επειδή",
+        "άμα",
+        "εάν",
+        "μολονότι",
+        "δηλαδή",
+        "λοιπόν",
+        "είτε",
+        "ότι",
+        "πωσ",
+        "μην",
+        "δεν",
+        "όταν",
+        "ώστε",
+    ];
+
+    prepos: string[] = [
+        "με",
+        "σε",
+        "στα",
+        "στη",
+        "στο",
+        "στην",
+        "στους",
+        "για",
+        "ως",
+        "πριν",
+        "προς",
+        "σαν",
+        "αντί",
+        "από",
+        "δίχως",
+        "έως",
+        "κατά",
+        "μετά",
+        "μέχρι",
+        "παρά",
+        "χωρίς",
+        "εναντίον",
+        "εξαιτίας",
+        "ίσαμε",
+        "μεταξύ",
+    ];
+
+    pronom: string[] = [
+        "εγώ",
+        "εσύ",
+        "αυτός",
+        "αυτή",
+        "αυτό",
+        "εμείς",
+        "εσείς",
+        "αυτοί",
+        "αυτές",
+        "αυτά",
+        "μου",
+        "σου",
+        "του",
+        "της",
+        "μας",
+        "σας",
+        "τους",
+        "εκείνος",
+        "ποιος",
+        "ποιο",
+        "ποιου",
+        "ποιανού",
+        "πολλά",
+        "πολλές",
+        "πολλοί",
+        "ποιο",
+        "τίνος",
+        "ποιων",
+        "ποιανών",
+        "ποιον",
+        "κανένας",
+        "κανείς",
+        "καμιά",
+        "καμία",
+        "κανένα",
+        "κάποιος",
+        "κάτι",
+        "κάθε",
+        "καθένας",
+        "καθείς",
+        "όλος",
+        "ίδιος",
+        "μόνος",
+        "άλλος",
+        "μερικοί",
+        "όποιος",
+        "όσος",
+        "τι",
+        "που",
+        "οποίος",
+        "εαυτό",
+        "αυτόν",
+        "αυτήν",
+        "εαυτός",
+        "εαυτού",
+        "εαυτά",
+        "όλα",
+        "όλες",
+        "όλη",
+        "όλο",
+    ];
+
+    nounEndings: string[] = [
         "η",
         "ή",
         "α",
@@ -58,8 +246,8 @@ export class StringWorker {
         "ές",
         "ως",
         "ώς",
-        "μα",
-        "μά",
+        "τά",
+        "τα",
         "ον",
         "όν",
         "εν",
@@ -84,12 +272,13 @@ export class StringWorker {
         "ούς",
         "ιμο",
         "ίμο",
-        "ματος",
-        "μάτος",
-        "μάτων",
+        "τος",
+        "τος",
+        "των",
+        "των",
     ];
 
-    adjectEndings = [
+    adjectEndings: string[] = [
         "α",
         "ά",
         "η",
@@ -114,15 +303,22 @@ export class StringWorker {
         "ών",
         "ους",
         "ούς",
-        "ικο",
-        "ίκο",
-        "ικου",
-        "ίκου",
     ];
 
-    proverbEndings = ["α", "ά", "ον", "όν", "ων", "ών", "ως", "ώς", "ε", "η"];
+    proverbEndings: string[] = [
+        "α",
+        "ά",
+        "ον",
+        "όν",
+        "ων",
+        "ών",
+        "ως",
+        "ώς",
+        "ε",
+        "η",
+    ];
 
-    verbEndings = [
+    verbEndings: string[] = [
         "ω",
         "ώ",
         "άω",
@@ -132,92 +328,263 @@ export class StringWorker {
         "ει",
         "εί",
         "άει",
-        "ουμε",
-        "ούμε",
-        "άμε",
-        "έτε",
-        "είτε",
-        "ετε",
-        "άτε",
-        "ατε",
+        "με",
+        "τε",
         "ουν",
-        "ουνε",
+        "νε",
         "ούν",
-        "ούνε",
         "αν",
         "άν",
-        "ανε",
-        "άνε",
         "α",
         "ες",
         "ε",
-        "αμε",
-        "άμε",
-        "ομαι",
-        "όμαι",
-        "εσαι",
-        "έσαι",
-        "εται",
-        "έται",
-        "αστε",
-        "άστε",
-        "εστε",
-        "έστε",
-        "ονται",
-        "όνται",
+        "μαι",
+        "ται",
+        "στε",
+        "νται",
+        "ται",
+        "σω",
+        "σουμε",
+        "σετε",
+        "σατε",
+        "σαμε",
     ];
 
     constructor(string: string) {
-        this.allWordsArr = string.split(/[\s,.]/);
+        this.allWordsArr = this.cleanWords(string);
     }
 
-    getUniqueWords() {
-        return new Set(this.allWordsArr);
+    getList(
+        dir: Sort = "desc",
+        simple: boolean = true,
+        mode: "txt" | "console" | "get" = "txt",
+        wordCount?: number | undefined
+    ): WordStructure[] {
+        let unqArr = this.#sortArray(dir);
+        let result = "";
+        let simpleMode = simple ? "-simple" : "-full";
+
+        if (wordCount) {
+            wordCount = wordCount > unqArr.length ? unqArr.length : wordCount;
+        } else {
+            wordCount = unqArr.length;
+        }
+
+        result += "Total number of words: " + unqArr.length + ".\n\t";
+
+        if (simple) {
+            unqArr.forEach((item) => {
+                let word = Object.keys(item.words)[0];
+                let rank = item.rank;
+
+                result += "\n\t" + word + ": " + rank + ";";
+            });
+        } else {
+            for (let i = 0; i < unqArr.length; i++) {
+                result += "\n\t" + unqArr[i].base + ": " + unqArr[i].rank + ";";
+
+                let arr = Object.entries(unqArr[i].words);
+
+                for (let u = 0; u < arr.length; u++) {
+                    result += "\n\t-- " + arr[u][0] + ": " + arr[u][1] + ";";
+                }
+            }
+        }
+
+        if (mode === "txt") {
+            fs.writeFile(`./file${simpleMode}.txt`, result, (err) =>
+                console.error(err)
+            );
+        }
+
+        if (mode === "console") {
+            console.log(result);
+        }
+
+        return unqArr;
     }
 
-    checkWord(word: string): string {
-        let message = word + " may be ";
+    cleanWords(str: string): string[] | never {
+        const arr = str.split(/[\s,.]/);
+        const cleanArr: string[] = [];
 
-        let wordEnding = this.#wordEnding(word);
-        console.log("wordEnding: " + wordEnding);
+        arr.forEach((word) => {
+            let clWord = word.toLowerCase().trim();
+
+            if (
+                word.length > 1 &&
+                word.match(/^[α-ωΑ-Ω]+/) &&
+                this.#censorship(clWord)
+            ) {
+                cleanArr.push(clWord);
+            }
+        });
+
+        if (cleanArr.length === 0) {
+            throw new Error("There is no words in your sentence!");
+        } else {
+            return cleanArr;
+        }
+    }
+
+    getUniqueWords(): Word {
+        let newObj: Word = {};
+
+        this.allWordsArr.forEach((item) => {
+            if (item in newObj) {
+                newObj[item] += 1;
+            } else {
+                newObj[item] = 1;
+            }
+        });
+
+        return newObj;
+    }
+
+    #censorship(word: string): boolean {
+        if (this.articles.includes(word.toLowerCase().trim())) {
+            return false;
+        }
+        if (this.conjunc.includes(word.toLowerCase().trim())) {
+            return false;
+        }
+        if (this.prepos.includes(word.toLowerCase().trim())) {
+            return false;
+        }
+        if (this.pronom.includes(word.toLowerCase().trim())) {
+            return false;
+        }
+        if (this.exept.includes(word.toLowerCase().trim())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    #sortArray(dir: Sort): WordStructure[] {
+        let unqArr = this.#compactWords(Object.entries(this.getUniqueWords()));
+
+        unqArr.sort((a, b) => {
+            switch (dir) {
+                case "desc":
+                    return b.rank - a.rank;
+
+                case "asc":
+                    return a.rank - b.rank;
+
+                case "name":
+                    if (a.base.toLowerCase() < b.base.toLowerCase()) {
+                        return -1;
+                    } else if (a.base.toLowerCase() > b.base.toLowerCase()) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+            }
+        });
+
+        return unqArr;
+    }
+
+    #checkEnding(ending: string): boolean {
+        let wordEnding = ending;
 
         if (this.nounEndings.includes(wordEnding)) {
-            message += "or a noun (" + wordEnding + ") ";
+            return true;
         }
 
         if (this.adjectEndings.includes(wordEnding)) {
-            message += "or an adjective (" + wordEnding + ") ";
+            return true;
         }
 
         if (this.proverbEndings.includes(wordEnding)) {
-            message += "or a proverb (" + wordEnding + ") ";
+            return true;
         }
 
         if (this.verbEndings.includes(wordEnding)) {
-            message += "or a verb (" + wordEnding + ") ";
+            return true;
         }
 
-        return message;
+        return false;
+    }
+
+    #compactWords(arr: Array<[string, number]>): Array<WordStructure> {
+        const wordBases: string[] = [];
+        const compactArr: Array<WordStructure> = [];
+
+        arr.forEach((item) => {
+            let word = item[0];
+            let rank = item[1];
+            const wordEnding = this.#wordEnding(word);
+            const wordBase = this.#wordBase(word, wordEnding);
+
+            const wordsObj: Word = {};
+            wordsObj[word] = rank;
+
+            if (!wordBases.includes(wordBase)) {
+                wordBases.push(wordBase);
+
+                let obj = {
+                    base: wordBase,
+                    words: wordsObj,
+                    rank: rank,
+                };
+                compactArr.push(obj);
+            } else {
+                compactArr.forEach((item) => {
+                    if (item.base === wordBase) {
+                        item.words[word] = rank;
+                        item.rank += rank;
+                    }
+                });
+            }
+        });
+
+        return compactArr;
     }
 
     #wordEnding(word: string): string {
         let wordEnding = "";
-        let flagEnding = false;
 
-        for (let i = word.length - 1; i > 0; i--) {
+        for (let i = word.length - 1; i > 1; i--) {
+            //Если гласный: добавляем его
             if (this.vocals.includes(word[i])) {
                 wordEnding += word[i];
-                flagEnding = true;
+
+                //Если не гласный: проверяем его
             } else {
-                if (!flagEnding) {
+                //Если согласный первый: добавим его.
+                if (!wordEnding) {
                     wordEnding += word[i];
+
+                    //Если согласный не первый: проверим на новое окончание
                 } else {
-                    wordEnding = wordEnding.split("").reverse().join("");
-                    break;
+                    let wordEndingCheck = wordEnding + word[i];
+                    wordEndingCheck = wordEndingCheck
+                        .split("")
+                        .reverse()
+                        .join("");
+
+                    //Если новое окончание есть в окончаниях спряжений и склонений: добавим букву
+                    if (this.#checkEnding(wordEndingCheck)) {
+                        wordEnding += word[i];
+
+                        //Если буквы нет в окончаниях спряжений и склонений: получаем окончание без согласного и прекращаем работу функции
+                    } else {
+                        wordEnding = wordEnding.split("").reverse().join("");
+                        break;
+                    }
                 }
             }
         }
 
         return wordEnding;
+    }
+
+    #wordBase(word: string, wordEnding: string): string {
+        let wordBaseNumber = wordEnding.length;
+        let wordBase = word.slice(0, -wordBaseNumber);
+
+        return wordBase;
     }
 }
---
+++ types/WordStructure.ts
@@ -0,0 +1,7 @@
+import Word from "./Word";
+
+export default interface WordStructure {
+    base: string;
+    words: Word;
+    rank: number;
+}
